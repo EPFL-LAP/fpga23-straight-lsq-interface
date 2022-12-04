4abd772285f79f90239c59d4a7d2de48188501da
ayaelakhras <ayatallah.elakhras@epfl.ch>
2022-12-04 22:37:18 +0100

    updating the baselines

--- experiments/baselines/constrained-fast-delivery/benchmarks/covariance/sim/C_SRC/new_covariance.h
+++
@@ -1,6 +0,0 @@
-typedef int inout_int_t;
-typedef int out_int_t;
-
-#define N 32
-
-int new_covariance(inout_int_t data[N][N], inout_int_t cov[N][N]);
--- experiments/baselines/constrained-fast-delivery/benchmarks/jacobi_1d_imper/sim/C_SRC/jacobi_1d_imper.h
+++
@@ -1,8 +0,0 @@
-
-typedef int inout_int_t;
-
-#define TSTEPS 3
-#define N 100
-
-int jacobi_1d_imper(inout_int_t A_1[N], inout_int_t A_2[N], inout_int_t B[N]);
-

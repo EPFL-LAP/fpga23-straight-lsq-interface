8bb7b931e71291c7f6c979d3034965935fa2f29a
ayaelakhras <ayatallah.elakhras@epfl.ch>
2022-12-04 16:56:25 +0100

    removed an outdated elastic-circuits pass

--- plugins/elastic-circuits/OptimizeBitwidth/printf.cpp
+++
@@ -1,3 +0,0 @@
-#include "OptimizeBitwidth/printf.h"
-
-size_t my_printf(llvm::raw_fd_ostream& stream, const char* format) { stream << format; }
--- plugins/elastic-circuits/include/MemElemInfo/MemUtils.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include "llvm/IR/Instructions.h"
-
-using namespace llvm;
-
-/// Find the array base for the memory access instruction I
-const Value* findBase(const Instruction* I);

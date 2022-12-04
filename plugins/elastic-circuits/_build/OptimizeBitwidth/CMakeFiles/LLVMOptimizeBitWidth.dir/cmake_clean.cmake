accddd41789b71d1d2b1ebd066c4047a797f96b8
ayaelakhras <ayatallah.elakhras@epfl.ch>
2022-12-04 17:07:34 +0100

    added the elastic-circuits binary

---
+++ plugins/elastic-circuits/_build/OptimizeBitwidth/CMakeFiles/LLVMOptimizeBitWidth.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LLVMOptimizeBitWidth.dir/OptimizeBitwidth.cpp.o"
+  "CMakeFiles/LLVMOptimizeBitWidth.dir/info.cpp.o"
+  "CMakeFiles/LLVMOptimizeBitWidth.dir/deps.cpp.o"
+  "libLLVMOptimizeBitWidth.pdb"
+  "libLLVMOptimizeBitWidth.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/LLVMOptimizeBitWidth.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
